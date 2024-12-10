bb8fb6a3a1388c117300ca2253944e8c0197ef0d
Amar Dev <devamar029@gmail.com>
2024-12-10 19:07:58 +0545

    banner updated

--- Frontend/src/pages/home/banner.tsx
+++ Frontend/src/pages/home/banner.tsx
@@ -1,7 +1,7 @@
 import { Button, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
-import bannerImg from "../../assets/images/banner.jpg"
-
+import { RightOutlined } from "@ant-design/icons";
+import bannerImg from "../../assets/images/banner.jpg";
 // interface BannerType {
 //   t: TFunction<"translation", undefined>;
 //   banner: {
@@ -18,37 +18,50 @@ import bannerImg from "../../assets/images/banner.jpg"
 //     updatedAt?: string;
 //   };
 // }
-
 const BannerComponent = () => {
   const { Title, Text } = Typography;
   // const { i18n } = useTranslation();
   const navigate = useNavigate();
-
   // const currentLang = i18n.language === "en" ? "En" : "Jp";
-
   return (
     <div className="banner">
       <div>
         {/* <img src={banner?.imageUrl} alt="Banner Image" /> */}
-        <img src={bannerImg} alt="Banner Image" />
+        <img
+          src={
+            "https://cdna.artstation.com/p/assets/images/images/068/029/138/large/anton-removskyi-2-final.jpg?1696841746"
+          }
+          alt="Banner Image"
+        />
         <div className="container">
           <div className="banner-info">
-            {/* <Title level={3}>{banner?.[`heading${currentLang}`]}</Title> */}
-            <Title level={3}>Nepal Designers And Builders</Title>
             <Text className="banner-info--text">
-              We Design. We Develop
+              <span className="dot"></span>
+              <span className="text-blue">We Design,</span>{" "}
+              <span className="text-red">We Develop</span>
             </Text>
+            <Title style={{ margin: "0" }} level={3}>
+              Nepal Designers & Builders
+            </Title>
             <Button
               type="primary"
               size="large"
               onClick={() => navigate("/about")}
             >
-            Know more !!!
+              <RightOutlined /> Know More
             </Button>
           </div>
         </div>
+        {/* Side Button */}
+        <Button
+          className="side-button"
+          onClick={() => alert("Side Button Clicked")}
+        >
+          Click Here
+        </Button>
       </div>
     </div>
   );
 };
+
 export default BannerComponent;
