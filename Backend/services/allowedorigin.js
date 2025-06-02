abeb77d651c18bc4f7232929d6eca5f4a6766be4
Nischal Saru <mnischal29@gmail.com>
2025-06-02 20:12:34 +0545

    add origin

--- Backend/services/allowedorigin.js
+++ Backend/services/allowedorigin.js
@@ -3,6 +3,7 @@ const allowOrigin = [
     origin: [
       "http://localhost:3000",
       "https://nepaldesignersandbuilders.netlify.app",
+      "https://nd-nb.netlify.app/",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
