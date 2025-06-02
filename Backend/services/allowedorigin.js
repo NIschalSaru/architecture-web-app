4321ec666fd40da6a4b69e8d7f7c1f3a94611a9e
Nischal Saru <100519474+NIschalSaru@users.noreply.github.com>
2025-06-02 20:27:14 +0545

    Update allowedorigin.js

--- Backend/services/allowedorigin.js
+++ Backend/services/allowedorigin.js
@@ -1,8 +1,8 @@
 const allowOrigin = {
   origin: [
     "http://localhost:3000",
-    // "https://nepaldesignersandbuilders.netlify.app",
-    "https://nd-nb.netlify.app/",
+    "https://nepaldesignersandbuilders.netlify.app",
+    "https://nd-nb.netlify.app",
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
